b65bededbad2b6d0a39d330369167c99e67acde0
Irvan Dawam Multazam <142770376+vandawam@users.noreply.github.com>
2026-05-09 15:42:37 +0700

    Replace global variable with time limit for spam
    Refactor spam functionality to use a time limit instead of a global variable.

--- src/pages/defg.lua
+++ src/pages/defg.lua
@@ -1,9 +1,7 @@
-getgenv().SpamKey1 = false
-
+local time = 0
 print("⌨️ Spam Tombol '1' Aktif!")
 
-task.spawn(function()
-    while getgenv().SpamKey1 do
+    while time <=200 do
         if keypress and keyrelease then
             -- 0x31 adalah kode Hex untuk tombol angka '1' di keyboard
             keypress(0x31) 
@@ -16,9 +14,9 @@ task.spawn(function()
         end
         
         -- Jeda antar tekanan tombol
+        time = time +1
         task.wait(0.05) 
     end
-end)
 
 -- CARA MEMATIKAN:
 -- Jalankan kode ini di executor untuk berhenti:
